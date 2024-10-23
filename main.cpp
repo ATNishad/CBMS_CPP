92db878ceb5548b7387dc8189f5f571badb25022
Nishad <thanveer2893@gmail.com>
2024-10-23 11:18:02 +0530

    encapsulated userhandling and message handling separately

--- main.cpp
+++ main.cpp
@@ -21,40 +21,24 @@ struct sender_structure {
   }
 };
 
-const string timer() {
-  const auto now = chrono::system_clock::now();
-  time_t t = chrono::system_clock::to_time_t(now);
-  string timestamp = std::ctime(&t);
-  timestamp.pop_back();
-  return timestamp;
-}
-
-void user_menu() {
-  cout << "\n";
-  cout << "-----MENU-----\n";
-  cout << "1.REGISTER-new user.\n";
-  cout << "2.LOGIN-existing user.\n";
-  cout << "3.Display users.\n";
-  cout << "4.EXIT.\n";
-  cout << "Enter your choice:";
-}
-void msg_menu() {
-  cout << "\n-----MENU-----\n";
-  cout << "1. Send Messages.\n";
-  cout << "2. View Messages.\n";
-  cout << "3. Edit Message.\n";
-  cout << "4. Delete Message.\n";
-  cout << "5. Save & Exit.\n";
-  cout << "Enter your choice: ";
-}
-
+class userHandling{
+public:
 void user_register(vector<sender_structure> &sender_vector) {
   string name;
+  while(true){
   cout << "Enter name to register:";
   getline(cin, name);
+  if(!name.empty()){
   sender_structure sender_object;
   sender_object.set_sender(name);
   sender_vector.push_back(sender_object);
+  cout<<"Registered successfully!\n";
+  break;
+  }
+  else{
+    cout<<"Enter valid name to register!!\n";
+  }
+}
 }
 
 void user_login(vector<sender_structure> &sender_vector,bool &global_user_logged,sender_structure *&current_user_pointer) {
@@ -77,6 +61,19 @@ for(const auto itr : sender_vector){
   cout<<",";
 }
   cout<<"\n";
+}
+
+};
+
+class messageHandling{
+public:
+
+const string timer() {
+  const auto now = chrono::system_clock::now();
+  time_t t = chrono::system_clock::to_time_t(now);
+  string timestamp = std::ctime(&t);
+  timestamp.pop_back();
+  return timestamp;
 }
 
 void send_message(vector<sender_structure> &sender_vector,sender_structure* current_user_pointer){
@@ -167,6 +164,27 @@ void loadfile(vector<sender_structure> &sender_vec, sender_structure* current_us
     cout << "Messages loaded!" << endl;
 }
 
+};
+
+void user_menu() {
+  cout << "\n";
+  cout << "-----MENU-----\n";
+  cout << "1.REGISTER-new user.\n";
+  cout << "2.LOGIN-existing user.\n";
+  cout << "3.Display users.\n";
+  cout << "4.EXIT.\n";
+  cout << "Enter your choice:";
+}
+void msg_menu() {
+  cout << "\n-----MENU-----\n";
+  cout << "1. Send Messages.\n";
+  cout << "2. View Messages.\n";
+  cout << "3. Edit Message.\n";
+  cout << "4. Delete Message.\n";
+  cout << "5. Save & Exit.\n";
+  cout << "Enter your choice: ";
+}
+
 
 
 int main() {
@@ -175,7 +193,12 @@ int main() {
   bool global_user_logged = false;
   vector<sender_structure> sender_vector;
   sender_structure *current_user_pointer = nullptr;
+  
+  userHandling userHandlingObject;
+  messageHandling messageHandlingObject;
 
+
+  
   // user_menu_do
   do {
     user_menu();
@@ -183,13 +206,13 @@ int main() {
     cin.ignore();
     switch (user_choice) {
     case 1:
-      user_register(sender_vector);
+      userHandlingObject.user_register(sender_vector);
       break;
 
     case 2:
-      user_login(sender_vector, global_user_logged, current_user_pointer);
+      userHandlingObject.user_login(sender_vector, global_user_logged, current_user_pointer);
       if(global_user_logged){
-      loadfile(sender_vector,current_user_pointer);
+      messageHandlingObject.loadfile(sender_vector,current_user_pointer);
       cout<<"LOGIN SUCCESSFULL!\n";
       }
       else{
@@ -198,7 +221,7 @@ int main() {
       break;
 
     case 3:
-      user_display(sender_vector);
+      userHandlingObject.user_display(sender_vector);
       break;
     case 4:
       cout << "Exited program.";
@@ -213,23 +236,23 @@ int main() {
     cin.ignore();
     switch (msg_choice) {
       case 1:
-      send_message(sender_vector,current_user_pointer);
+      messageHandlingObject.send_message(sender_vector,current_user_pointer);
       break;
 
       case 2:
-      view_message(sender_vector,current_user_pointer);
+      messageHandlingObject.view_message(sender_vector,current_user_pointer);
       break;
       
       case 3:
-      edit_message(sender_vector,current_user_pointer);
+      messageHandlingObject.edit_message(sender_vector,current_user_pointer);
       break;
       
       case 4:
-      delete_message(sender_vector,current_user_pointer);
+      messageHandlingObject.delete_message(sender_vector,current_user_pointer);
       break;
     
       case 5:
-      savefile(sender_vector,current_user_pointer);
+      messageHandlingObject.savefile(sender_vector,current_user_pointer);
       break;
     }
   } while (msg_choice != 5);
